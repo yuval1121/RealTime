1e61f85d4debe24f3ec242ceae34607d90057ffb
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-12-07 00:31:32 +0200

    Added food URL

--- RealTime/RealTime/urls.py
+++ RealTime/RealTime/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+
+from django.contrib import admin
+from django.urls import include,path
 
 urlpatterns = [
-    # Uncomment the next line to enable the admin:
-    #path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('food/',include('food.urls')),
 ]
