2f96cc494e4513c28d4ad970a7b7cdc13b84a970
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-12-14 03:14:12 +0200

    Added a few things

--- RealTime/food/urls.py
+++ RealTime/food/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
 ]
