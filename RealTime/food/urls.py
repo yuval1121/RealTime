19c6cd379158e60967301dad41ff6a0d9fdd9fd0
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-12-14 04:53:35 +0200

    added dynamic url

--- RealTime/food/urls.py
+++ RealTime/food/urls.py
@@ -2,7 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    #/food/
     path('',views.index,name='index'),
+    #/food/1
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
 ]
