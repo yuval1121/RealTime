2d817093fd49dc8b2be748aeb0c4004e56e358e9
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2020-12-31 20:24:20 +0200

    Added images and a default image

--- RealTime/food/models.py
+++ RealTime/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://designshack.net/wp-content/uploads/placeholder-image-368x246.png")
