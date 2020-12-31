92794e9f200a7a3f21ac240d4926f7dced4d9f59
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-01 01:56:48 +0200

    Added store logic

--- RealTime/food/models.py
+++ RealTime/food/models.py
@@ -8,5 +8,10 @@ class Item(models.Model):
 
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
-    item_price = models.IntegerField()
     item_image = models.CharField(max_length=500,default="https://designshack.net/wp-content/uploads/placeholder-image-368x246.png")
+    item_opening_hours = models.CharField(max_length = 10,default="07:00")
+    item_shipping = models.BooleanField(default=False)
+    item_shipping_avail = models.BooleanField(default=False)
+    item_sales = models.CharField(max_length=200,default="None")
+    item_allowed_people = models.IntegerField(default=30)
+    item_people_inside = models.IntegerField(default=0)
