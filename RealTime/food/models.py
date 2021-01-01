3f1e0ab26b1654d19d9645945f9b4876919619da
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-01 21:29:02 +0200

    Renamed availablity

--- RealTime/food/models.py
+++ RealTime/food/models.py
@@ -11,7 +11,7 @@ class Item(models.Model):
     item_image = models.CharField(max_length=500,default="https://designshack.net/wp-content/uploads/placeholder-image-368x246.png")
     item_opening_hours = models.CharField(max_length = 10,default="07:00")
     item_shipping = models.BooleanField(default=False)
-    item_shipping_avail = models.BooleanField(default=False)
+    item_shipping_available = models.BooleanField(default=False)
     item_sales = models.CharField(max_length=200,default="None")
     item_allowed_people = models.PositiveIntegerField(default=30)
     item_people_inside = models.PositiveIntegerField(default=1)
