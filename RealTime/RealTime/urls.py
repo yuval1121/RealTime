40ffaeb3f1e1ca7174eaeb96a59c6c3328eb6736
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-02 01:14:56 +0200

    Added registeration

--- RealTime/RealTime/urls.py
+++ RealTime/RealTime/urls.py
@@ -19,8 +19,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include,path
+from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('food.urls')),
+    path('register/',user_views.register,name="register"),
 ]
