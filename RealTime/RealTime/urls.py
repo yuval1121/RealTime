f336a54e87c6931b2128f2fa17ebdb25e5687a91
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-03 06:49:28 +0200

    Revert "Added image to profile"
    This reverts commit 5e3373997a5f9b7a4f533c2ec60151455a5ce4a6.

--- RealTime/RealTime/urls.py
+++ RealTime/RealTime/urls.py
@@ -22,8 +22,6 @@ from django.urls import include,path
 from users import views as user_views
 from django.contrib.auth import views
 from django.contrib.auth import views as authentication_views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,9 +32,3 @@ urlpatterns = [
     path('profile/',user_views.profilepage,name='profile'),
 
 ]
-
-
-
-urlpatterns += [
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
