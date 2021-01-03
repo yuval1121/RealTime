374e2f429a7105cd7333924608c436cb391105f4
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-03 20:40:54 +0200

    index file fix

--- RealTime/food/models.py
+++ RealTime/food/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
@@ -6,6 +7,7 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
 
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_image = models.CharField(max_length=500,default="https://designshack.net/wp-content/uploads/placeholder-image-368x246.png")
