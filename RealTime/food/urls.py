4c01e9bb7aa8ef9e86e44da9826174abf143dd1b
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-03 18:34:51 +0200

    Changed index to class view

--- RealTime/food/urls.py
+++ RealTime/food/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name='food'
 urlpatterns = [
     #/food/
-    path('',views.index,name='index'),
+    path('',views.IndexClassView.as_view(),name='index'),
     #/food/1
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
--- RealTime/food/views.py
+++ RealTime/food/views.py
@@ -3,15 +3,14 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
 # Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-    
-    context = {
-        'item_list':item_list,
-        }
-    return render(request,'food/index.html',context)
+class IndexClassView(ListView):
+    model = Item;
+    template_name='food/index.html'
+    context_object_name ='item_list'
+
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
