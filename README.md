ef453a80411e01f867fcf12b8fcbed5b87616bb4
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-04 22:01:50 +0200

    fixed README, added test and added info to detail page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Real Time
+RealTime
 
-Realtime is a simple CRUD web app made in django to keep track of stores and how many people are
+RealTime is a simple CRUD web app made in django to keep track of stores and how many people are
 in them during lockdown times.
 
 Superuser credentials:
--- RealTime/food/tests.py
+++ RealTime/food/tests.py
@@ -5,8 +5,12 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+
 import django
 from django.test import TestCase
+from django.urls import reverse,resolve
+
+
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -18,9 +22,12 @@ class SimpleTest(TestCase):
     def setUpClass(cls):
         super(SimpleTest, cls).setUpClass()
         django.setup()
+        
 
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        from .models import Item
+        self.a = Item(item_name="Hello")
+        self.assertEqual(self.a.item_name,"Hello")
