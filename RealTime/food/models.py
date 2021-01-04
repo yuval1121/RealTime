730207b067c1e67e13a0991356230e6e09656c38
yuval1121 <56655907+yuval1121@users.noreply.github.com>
2021-01-04 16:36:59 +0200

    Fixed opening hours

--- RealTime/food/models.py
+++ RealTime/food/models.py
@@ -12,7 +12,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_image = models.CharField(max_length=500,default="https://designshack.net/wp-content/uploads/placeholder-image-368x246.png")
-    item_opening_hours = models.CharField(max_length = 10,default="07:00")
+    item_opening_hours = models.CharField(max_length = 12,default="07:00-21:00")
     item_shipping = models.BooleanField(default=False)
     item_shipping_available = models.BooleanField(default=False)
     item_sales = models.CharField(max_length=200,default="None")
